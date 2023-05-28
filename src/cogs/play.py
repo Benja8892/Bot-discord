ea8bb40ba2871c4019cc5adb735dca807ddf338b
Benjamin <benjabossca@gmail.com>
2023-05-28 01:45:23 -0300

    Add more functions to bot

--- src/cogs/play.py
+++ src/cogs/play.py
@@ -32,21 +32,20 @@ class PlayerBotMusic(commands.Cog):
       self.client = client
       self.state = MusicPlayerState.DEAD
       self.voice_client = None
+      self.song_requested = []
 
     @commands.command()
     async def play(self, ctx, *, search):
-      self.search = search
-
+      self.search = search   
       YDL_OPTIONS = {'format': 'bestaudio/best',
                         'noplaylist': 'True',
                         'postprocessors': [{
                                 'key': 'FFmpegExtractAudio',
                                 'preferredcodec': 'opus',
             }]}
-      
+
       if not ctx.author.voice:
           await ctx.send("You are not connected to a voice channel.")
-
 
       """Si el estado esta en STOPPED no """
       if self.state == MusicPlayerState.DEAD:
@@ -55,31 +54,36 @@ class PlayerBotMusic(commands.Cog):
         self.state = MusicPlayerState.PLAYING
 
 
-      #Convierte search en una consulta lista para mandar a una URL para realizar una búsqueda en Youtube.
-      query_string = parse.urlencode({'search_query': self.search})
+      if self.state in [MusicPlayerState.PLAYING, MusicPlayerState.WAITING]:
+        #Convierte search en una consulta lista para mandar a una URL para realizar una búsqueda en Youtube.
 
-      #en request pido todos los videos que se parezcan a querystring
-      html_content = request.urlopen(f'http://www.youtube.com/results?search_query={query_string}')
+        if self.search[0:4] != "http:":
+          query_string = parse.urlencode({'search_query': self.search})
 
-      #En esta busco en el html recibido cual es el link de watch para enviarlo en el ctx.send
-      search_results = re.findall(r'watch\?v=(\S{11})',html_content.read().decode())
-      await ctx.send("https://www.youtube.com/watch?v=" + search_results[0])
-      url = "https://www.youtube.com/watch?v=" + search_results[0]
+          #en request pido todos los videos que se parezcan a querystring
+          html_content = request.urlopen(f'http://www.youtube.com/results?search_query={query_string}')
+
+          #En esta busco en el html recibido cual es el link de watch para enviarlo en el ctx.send
+          search_results = re.findall(r'watch\?v=(\S{11})',html_content.read().decode())
+          await ctx.send("https://www.youtube.com/watch?v=" + search_results[0])
+          url = "https://www.youtube.com/watch?v=" + search_results[0]
+          
+        else:
+          url = self.search
+
+        """Llamos a la libreria, extraemos la info de la url y mandamos a que reproduzca el audio"""
+        with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+            audio_url = info['url']
+            source = await discord.FFmpegOpusAudio.from_probe(audio_url, method="fallback")
+        self.voice_client.play(source=source)
 
 
-      """Llamos a la libreria, extraemos la info de la url y mandamos a que reproduzca el audio"""
-      with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
-          info = ydl.extract_info(url, download=False)
-          audio_url = info['url']
-          source = await discord.FFmpegOpusAudio.from_probe(audio_url, method="fallback")
-      self.voice_client.play(source=source)
-
-
-      voice = get(self.client.voice_clients, guild=ctx.guild)
-      if voice.is_playing():
-          ctx.send(await ctx.message.add_reaction('🎵')) # reaccion para confirmar que se está reproduciendo la canción
-      else:
-          ctx.send("ERROR") 
+        voice = get(self.client.voice_clients, guild=ctx.guild)
+        if voice.is_playing():
+            ctx.send(await ctx.message.add_reaction('🎵')) # reaccion para confirmar que se está reproduciendo la canción
+        else:
+            ctx.send("ERROR") 
 
 
 
@@ -93,9 +97,11 @@ class PlayerBotMusic(commands.Cog):
         self.voice_client = await voice_channel.connect()
         self.state = MusicPlayerState.WAITING
 
-      else:
+      elif self.voice is not None:
          await self.voice_client.move_to(voice_channel)
-      
+
+      else:
+        await ctx.send("You don't use this command because the bot is already entry")
 
 
     @commands.command()
@@ -121,6 +127,8 @@ class PlayerBotMusic(commands.Cog):
       elif self.state == MusicPlayerState.STOPPED:
          ctx.send("No hay nada reproduciendose UwU")
 
+
+
     @commands.command()
     async def resume(self, ctx):
         if not ctx.author.voice:
@@ -129,6 +137,19 @@ class PlayerBotMusic(commands.Cog):
         if self.state == MusicPlayerState.STOPPED:
           self.voice_client.resume()
           self.state = MusicPlayerState.PLAYING
+
+    @commands.command()
+    async def skip(self, ctx):
+      if not ctx.author.voice:
+          await ctx.send("You are not connected to a voice channel.")
+
+      if len(self.song_requested) > 0:
+        next_song = self.song_requested.pop(0)
+        self.voice = next_song
         
+         
+       
+
+
 async def setup(client:commands.Bot) -> None:
   await client.add_cog(PlayerBotMusic(client))
